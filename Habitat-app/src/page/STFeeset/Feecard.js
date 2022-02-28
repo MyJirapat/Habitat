0d74dd30b087c18dfac1fa026ceae39462e9905e
Jirapat Suwanjunee <suwanjunee987@gmail.com>
2022-02-28 16:12:53 +0700

    Delete import file

--- Habitat-app/src/page/STFeeset/Feecard.js
+++ Habitat-app/src/page/STFeeset/Feecard.js
@@ -31,16 +31,18 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+
   paper: {
     padding: theme.spacing(2),
     margin: 'auto',
     width: '100%',
-    
   },
+
   image: {
     width: 128,
     height: 128,
   },
+
   img: {
     margin: 'auto',
     display: 'block',
@@ -57,15 +59,18 @@ const useStyles = makeStyles((theme) => ({
   root2: {
     flexGrow: 1,
   },
+
   papers: {
     padding: theme.spacing(2),
     margin: 'auto',
     maxWidth: 500,
   },
+
   images: {
     width: 128,
     height: 128,
   },
+
   imgs: {
     margin: 'auto',
     display: 'block',
@@ -76,11 +81,8 @@ const useStyles = makeStyles((theme) => ({
   Dialog:{
     padding: theme.spacing(2),
     width: 450,
-    
   }
-
 }));
-
 
 export default function Feetype(props) {
   const classes = useStyles();
@@ -93,12 +95,10 @@ export default function Feetype(props) {
     setOpen(false);
   };
 
-
   useEffect(()=>{
     axios.get('/feeset/fee-sets').then(response =>{
       console.log(response.data);
       setAllfeeset(response.data);
-      
     })
     .catch(error => {
       console.log('Error data: ' + error);
@@ -108,87 +108,26 @@ export default function Feetype(props) {
 
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography  style={{ marginLeft: "-12%" }}
-                gutterBottom variant="h5">
-                  {props.Set} 
-                  <Button size="small">{props.info}</Button>   
-                {/*<Button 
-                        onClick={handleClickOpen} size="small">
-                <InfoIcon size="small"/>{props.dialogaction}</Button>*/}
-                    
-     {/* <Dialog open={open} onClose={handleClose} 
-              aria-labelledby="edit-apartment">
-        
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>Set</DialogTitle>
-        <DialogContent className={classes.Dialog}>
-          <DialogContentText>
-            <form >
-            <Grid container spacing={2}>
-              <Grid item xs={5}>
-                  <Typography gutterBottom variant="subtitle1">
-                    {props.name}
-                  </Typography>
-                  <Typography gutterBottom variant="subtitle1">
-                    {props.name}
-                  </Typography>
-              
+        <Paper className={classes.paper}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                    <Grid item xs>
+                        <Typography style={{ marginLeft: "-12%" }} gutterBottom variant="h5">{props.Set} 
+                          <Button size="small">{props.info}</Button>   
+                        </Typography>
+                        <Typography variant="body2" gutterBottom></Typography>
+                        <Typography variant="body2" color="textSecondary">{props.Roomprice}</Typography>
+                        <Typography variant="body2" color="textSecondary">{props.Electric}</Typography>
+                        <Typography variant="body2" color="textSecondary">{props.Water}</Typography>
+                    </Grid>
+                    <Grid item><Typography variant="body2" style={{ cursor: 'pointer' }}></Typography></Grid>
               </Grid>
-          <Grid item xs={5}>
-            <Typography gutterBottom variant="subtitle1">
-                  {props.roomprice}
-                </Typography>
-               
-          </Grid>
-          <Grid item xs={2} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs={5}>
-                <Typography gutterBottom variant="subtitle1">
-                    {props.currency}
-                </Typography>
-              </Grid>
+                    <Grid item><Button size="small">{props.Edit}</Button></Grid> 
+                    <Grid item><Button size="small">{props.delete}</Button></Grid>         
             </Grid>
           </Grid>
-        </Grid>                    
-            <DialogActions className="createAccount">
-            </DialogActions>
-            </form>
-          </DialogContentText>
-        </DialogContent>      
-  </Dialog>*/}
-                </Typography>
-                <Typography variant="body2" gutterBottom>       
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  {props.Roomprice}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  {props.Electric}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  {props.Water}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-                <Button size="small">{props.Edit}</Button>
-            </Grid> 
-            <Grid item>
-                <Button size="small">{props.delete}</Button>
-            </Grid>  
-                   
-          </Grid>
-        </Grid>
-   
-      </Paper>
+        </Paper>
     </div>
   );
 }
--- Habitat-app/src/page/STFeeset/Feeset.js
+++ Habitat-app/src/page/STFeeset/Feeset.js
@@ -7,11 +7,9 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-
 import Table from '@material-ui/core/Table';
 import Grid from '@material-ui/core/Grid';
 import Feecard from './Feecard'
-// import Feecard from "../../Components/Feeset/Feecard";
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -19,12 +17,10 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { NavLink, Link } from "react-router-dom";
-//import Savebtn from "../../Components/Button/Save";
 import InfoIcon from '@material-ui/icons/Info';
 import axios from "axios";
 import { useParams } from "react-router";
 import CircularProgress from '@mui/material/CircularProgress';
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -145,97 +141,79 @@ export default function Feeset(props) {
   };
 
   const { id } = useParams()
+    if (isLoading) {
+    return (
+        <div className="container ">
+          <div>
+            <Card className={classes.Card} variant="outlined">
+                <Table aria-label="caption table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell className={classes.heder}>
+                        <h4>Fee Sets</h4>
+                        <NavLink to={`/feesets/${id}`} style={{ textDecoration: 'none' }}>
+                            <Button
+                                className={classes.Btn}
+                                variant="contained" color="primary" disableElevation
+                                style={{ backgroundColor: '#485D84' }}>Create</Button>
+                          </NavLink>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
 
-  if (isLoading) {
-   
-  return (
-    <div className="container ">
-      <div>
-
-        <Card className={classes.Card} variant="outlined">
-          <Table aria-label="caption table">
-            <TableHead >
-              <TableRow>
-                <TableCell className={classes.heder}>
-                  <h4>Fee Sets</h4>
-                  <NavLink to={`/feesets/${id}`} style={{ textDecoration: 'none' }}>
-                    {/* <Link to={`/feesets_sp/${id}`} style={{ textDecoration: 'none' }}> */}
-
-                    <Button
-                      className={classes.Btn}
-                      variant="contained" color="primary" disableElevation
-                      style={{ backgroundColor: '#485D84' }}>Create</Button>
-                  </NavLink>
-                  {/* </Link> */}
-
-                </TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              <br />
+                  <TableBody>
+                  <br />
               <Grid container spacing={0}>
-                {allfeeset.map((row, key) => {
-                  return (
-                    <Grid item xs={4}>
-                      <div className="col-sm">
-                        <Feecard 
-                          className={classes.MoveName}
-                          Set={row.feeSetName}
-                          //key={key.feeSetId}
-                          //Roomprice="Standard"
-                            Electric="Electricity"
-                            Water="water"
-                          name={row.feeTypeName}
-                          info={
-                            <InfoIcon style={{ display: "flex", flexWrap: 'wrap' }}
-                              onClick={handleClickOpen}
-                              size="small" />}
-                          /*Edit={
-                              <DeleteOutlinedIcon 
-                              onClick={()=>{deletetable(row.feeSetId)}}
-                                size="small"/>}*/
-
-                          delete={
-                            <DeleteOutlinedIcon
-                              style={{ marginTop: "12%" }}
-                              onClick={() => { deletetable(row.feeSetId) }}
-                              size="small" />}
-                        />     
-                      </div>
-                      <Dialog
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="edit-apartment">
-                        <DialogTitle id="customized-dialog-title" onClose={handleClose}>{row.feeSetName}</DialogTitle>
-
-                        <DialogContent className={classes.Dialog}>
-                          {row.feeTypes.map((num) => (
-                            <DialogContentText>
-                              <DialogActions className="createAccount">
-                                <Grid container spacing={2}>
-                                  <Grid item xs={5}>
-                                    {num.feeTypeName}
-                                  </Grid>
-                                  <Grid item xs={5}>
-                                    {num.feeTypePrice}
-                                  </Grid>
-                                  <Grid item xs={2} sm container>
-                                    <Grid item xs container direction="column" spacing={2}>
-                                      <Grid item xs={5}>
-                                        THB
-                                      </Grid>
-                                    </Grid>
-                                  </Grid>
-                                </Grid>
-                              </DialogActions>
-                            </DialogContentText>
-                          ))}
+                    {allfeeset.map((row, key) => {
+                      return (
+                        <Grid item xs={4}>
+                            <div className="col-sm">
+                                <Feecard 
+                                    className={classes.MoveName}
+                                    Set={row.feeSetName}
+                                    Electric="Electricity"
+                                    Water="water"
+                                    name={row.feeTypeName}
+                                    info={
+                                          <InfoIcon style={{ display: "flex", flexWrap: 'wrap' }}
+                                          onClick={handleClickOpen}
+                                          size="small" />}
+                                    delete={
+                                          <DeleteOutlinedIcon
+                                          style={{ marginTop: "12%" }}
+                                          onClick={() => { deletetable(row.feeSetId) }}
+                                          size="small" />}/>     
+                              </div>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="edit-apartment">
+                    <DialogTitle id="customized-dialog-title" onClose={handleClose}>{row.feeSetName}</DialogTitle>
+                    <DialogContent className={classes.Dialog}>
+                                  {row.feeTypes.map((num) => (
+                                  <DialogContentText>
+                                      <DialogActions className="createAccount">
+                                          <Grid container spacing={2}>
+                                                <Grid item xs={5}>
+                                                  {num.feeTypeName}
+                                                </Grid>
+                                                <Grid item xs={5}>
+                                                  {num.feeTypePrice}
+                                                </Grid>
+                                                <Grid item xs={2} sm container>
+                                                      <Grid item xs container direction="column" spacing={2}>
+                                                       <Grid item xs={5}>THB</Grid>
+                                                      </Grid>
+                                                </Grid>
+                                           </Grid>
+                                        </DialogActions>
+                                </DialogContentText>
+                                ))}
                         </DialogContent>
                       </Dialog>
                     </Grid>
-                  )
-                })}
+                    )
+                  })}
               </Grid>
             </TableBody>
           </Table>
